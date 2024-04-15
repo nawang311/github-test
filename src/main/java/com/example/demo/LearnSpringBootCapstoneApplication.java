43e2f14dfcb83c08efcc29c1b9303d108d78de39
simple_nickname <simplecodingTest458113@gmail.com>
2024-04-15 19:25:11 +0900

    test commit

---
+++ src/main/java/com/example/demo/LearnSpringBootCapstoneApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringBootCapstoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringBootCapstoneApplication.class, args);
+	}
+
+}
